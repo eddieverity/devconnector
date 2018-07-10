cf1709dd4e87b873c8161010aa3f01eb86ccf1b2
Edward Verity <eddieverity@gmail.com>
2018-07-10 17:10:06 -0500

    finished user routes

--- validation/login.js
+++ validation/login.js
@@ -1,1 +1,28 @@
-login.js
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // This is for if fields left blank, they're put in as empty strings for isEmpty function to run
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
+
+  if(Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+  
+  if(!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if(Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors: errors,
+    isValid: isEmpty(errors)
+  }
+}
--- validation/register.js
+++ validation/register.js
@@ -1,1 +1,49 @@
-register.js
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  // This is for if fields left blank, they're put in as empty strings for isEmpty function to run
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
+  if(!Validator.isLength(data.name, { min: 2, max: 30 })){
+    errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if(Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+
+  if(Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+  
+  if(!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if(Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  if(!Validator.isLength(data.password, {min: 6, max: 30})) {
+    errors.password = 'Password must be between 6 and 30 characters';
+  }
+
+  if(Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm Password field is required';
+  }
+
+  if(!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+  }
+
+  return {
+    errors: errors,
+    isValid: isEmpty(errors)
+  }
+}
