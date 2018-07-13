fe77d48f4c1c1bea06ad074c51f6a251c8594cfa
Edward Verity <eddieverity@gmail.com>
2018-07-13 09:38:21 -0500

    finished redux routes for profile

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
 
+
 class Login extends Component {
 
   constructor() {
@@ -16,7 +17,11 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-
+  componentDidMount() {
+    if(this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
 
   componentWillReceiveProps(nextProps) {
     if(nextProps.auth.isAuthenticated) {
